76ea207d2e7399ec30d8692e92b42c9189080fb3
einani <162230711+einani@users.noreply.github.com>
2024-03-04 22:08:48 -0800

    Followed tutorial for draft 1

---
+++ script.js
@@ -0,0 +1,7 @@
+// add or remove open class from these elements
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
